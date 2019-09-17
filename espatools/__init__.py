335ade38e56b7957a476b8ab7d3829278421ea51
banesullivan <banesulli@gmail.com>
2019-09-17 14:32:10 -0600

    Bump version: 0.0.8 → 0.1.0

--- espatools/__init__.py
+++ espatools/__init__.py
@@ -9,5 +9,5 @@ from .read import *
 __author__ = 'Bane Sullivan'
 __license__ = 'BSD-3-Clause'
 __copyright__ = '2018, Bane Sullivan'
-__version__ = '0.0.8'
+__version__ = '0.1.0'
 __displayname__ = 'ESPA Tools'
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 
 import setuptools
 
-__version__ = '0.0.8'
+__version__ = '0.1.0'
 
 with open("README.rst", "r") as f:
     long_description = f.read()
